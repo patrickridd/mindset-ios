5e93cf30ee40f184a95a9db00a7f8319348e3f6f
Patrick Ridd <patrickridd@gmail.com>
2026-01-07 21:15:26 -0700

    Added public init in PaywallView so our coordinator to access it

--- Packages/FeatureSubscription/Sources/FeatureSubscription/PaywallView.swift
+++ Packages/FeatureSubscription/Sources/FeatureSubscription/PaywallView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 public struct PaywallView: View {
     @Environment(\.dismiss) var dismiss
     
+    public init() {}
+    
     public var body: some View {
         ZStack {
             // Background: Subtle Gradient
