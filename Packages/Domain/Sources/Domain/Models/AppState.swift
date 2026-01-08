11fb33b8b648e894501f7f6808014c6473905d97
Patrick Ridd <patrickridd@gmail.com>
2026-01-07 20:46:13 -0700

    Add  enum for MainCoordinator

---
+++ Packages/Domain/Sources/Domain/Models/AppState.swift
@@ -0,0 +1,13 @@
+//
+//  AppState.swift
+//  Domain
+//
+//  Created by patrick ridd on 1/7/26.
+//
+
+/// AppState used for MainCoordinator Navigation
+public enum AppState {
+    case onboarding
+    case paywall
+    case dashboard
+}
