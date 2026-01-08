5d994f80892cdb1e0992b4539197f7c695790d67
Patrick Ridd <patrickridd@gmail.com>
2026-01-07 20:26:16 -0700

    Added

---
+++ Packages/Domain/Sources/Domain/Models/OnboardingData.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingData.swift
+//  Domain
+//
+//  Created by patrick ridd on 1/7/26.
+//
+
+public struct OnboardingData: Sendable {
+    public var goal: String = ""
+    public var overwhelmFrequency: String = ""
+    // ... other fields
+}
