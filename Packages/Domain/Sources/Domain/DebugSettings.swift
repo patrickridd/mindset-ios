ab6d75c04d6866157a3f632471a496e363803933
Patrick Ridd <patrickridd@gmail.com>
2026-03-01 10:03:13 -0700

    Created DebugSettings that tracks whether we should `useMocks` or not. We use the `UserDefault` propertyWrapper to store this Bool

---
+++ Packages/Domain/Sources/Domain/DebugSettings.swift
@@ -0,0 +1,19 @@
+//
+//  DebugSettings.swift
+//  Domain
+//
+//  Created by patrick ridd on 3/1/26.
+//
+
+import Foundation
+
+@MainActor
+public final class DebugSettings {
+    public static let shared = DebugSettings()
+
+    // Pure Foundation-based storage
+    @UserDefault(key: "com.mindset.debug.useMocks", defaultValue: false)
+    public var useMocks: Bool
+
+    private init() {}
+}
